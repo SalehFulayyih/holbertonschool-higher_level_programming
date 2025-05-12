07b056698bc01bd72cd2481831a43fc024715bbb
SalehFulayyih <saleh.5050@hotmail.com>
2025-05-12 13:00:09 +0300

    update file 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,3 +8,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
     return (a1 + b1, a2 + b2)
+
+
+'''
+ tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+
+    # Calculate the sum of the first and second elements
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
+'''
