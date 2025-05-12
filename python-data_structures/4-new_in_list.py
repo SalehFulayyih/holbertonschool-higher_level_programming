8358da04b7a10de33a1740a869d987bfcd7b415a
SalehFulayyih <saleh.5050@hotmail.com>
2025-05-12 12:17:31 +0300

    Add file 3-print_reversed_list_integer.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        newlist = my_list
+        newlist = my_list[element]
         return newlist
     return my_list
