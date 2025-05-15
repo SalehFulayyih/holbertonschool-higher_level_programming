13550d2273109f4071c0b7f742ea7921ff465c5d
SalehFulayyih <saleh.5050@hotmail.com>
2025-05-15 06:24:34 +0000

    Add file 102-magic_calculation.py

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+        except Exception:
+            break
+        else:
+            result += (a ** b) / i
+    else:
+        result = a + b
+    return result
