6c71e75aac91a1fddda6bdb4595b11aa5e8ef741
SalehFulyyih <saleh.5050@htomail.com>
2025-05-17 11:08:09 +0000

    Update some files

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,20 +5,9 @@ import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    """
-
+    """Return the multiplication of two matrices."""
     if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise ValueError("Both m_a and m_b must be lists")
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise ValueError("Both m_a and m_b must be lists of lists")
-    if not all(isinstance(item, (int, float)) for row in m_a for item in row):
-        raise ValueError("m_a must be a list of lists of integers or floats")
-    if not all(isinstance(item, (int, float)) for row in m_b for item in row):
-        raise ValueError("m_b must be a list of lists of integers or floats")
-
+        raise TypeError("Both m_a and m_b must be lists")
+    if isinstance(m_a, str) or isinstance(m_b, str):
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
     return np.matmul(m_a, m_b)
