bd3fff989b7a1ec6dfec94a594b7c91378857d6a
SalehFulayyih <saleh.5050@hotmail.com>
2025-05-17 15:14:27 +0300

    Update 101-lazy_matrix_mul.py

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.5
+"""
+
+Module composed by a function that multiplies 2 matrices
+
+"""
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices."""
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("Both m_a and m_b must be lists")
-    if isinstance(m_a, str) or isinstance(m_b, str):
-        raise ValueError("Scalar operands are not allowed, use '*' instead")
-    return np.matmul(m_a, m_b)
+    """ Function that multiplies 2 matrices
+
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+
+    Returns:
+        result of the multiplication
+
+
+    """
+
+    return (np.matmul(m_a, m_b))
