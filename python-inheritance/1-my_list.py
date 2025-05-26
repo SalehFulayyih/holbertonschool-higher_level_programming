4fce189a2c42ee1116bec3fcc5c387c71a1c9d58
SalehFulayih <saleh.5050@homail.com>
2025-05-26 14:18:56 +0000

    Add files 1-my_list.py, tests/1-my_list.txt

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that prints a sorted list inherited from list"""
+
+
+class MyList(list):
+    """prints a sorted list in an ascending order"""
+
+    def print_sorted(self):
+        """Prints a list in ascending order"""
+        print(sorted(self))
