58ce4cfcb89dcfa67222f915ba09ddd0d700cbd1
root <Saleh.5050@hotmail.com>
2025-06-24 12:21:29 +0000

    Add file 10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- 10-top_score.sql: lists score and name from second_table ordered by score descending
+
+SELECT 'score', 'name'
+UNION ALL
+SELECT score, name FROM second_table ORDER BY score DESC;
