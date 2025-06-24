7c2e59aca0785d34d7b7e03ef9f11496fbd139e1
root <Saleh.5050@hotmail.com>
2025-06-24 12:24:46 +0000

    Add file 11-best_score.sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,4 @@
 -- 11-best_score.sql : lists records with score >= 10, ordered by score descending, with header
 
-SELECT 'score', 'name'
-UNION ALL
+
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
