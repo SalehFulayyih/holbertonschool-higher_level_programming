279346268b567c7e4009142d1152758d7b13b3ea
SalehFulayyih <saleh.5050@hotmail.com>
2025-06-28 10:52:10 +0000

    Add file 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,11 @@
-from sys import argv
-import MySQLdb
-x  # !/usr/bin/python3
+#!/usr/bin/python3
 """
 lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa, safe from injection
 """
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
