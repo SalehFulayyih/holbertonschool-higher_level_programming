25b3144d56b5dcc553376a7179e0ad601a2b9e2a
SalehFulayyih <saleh.5050@hotmail.com>
2025-06-28 09:59:23 +0000

    Update file 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,6 @@
 -- Sort ascending by title and genre_id
 -- Only one SELECT statement allowed
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
