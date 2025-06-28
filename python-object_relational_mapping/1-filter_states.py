665aa576287126983af9401ad24e7653199e4790
SalehFulayyih <saleh.5050@hotmail.com>
2025-06-28 10:42:00 +0000

    Add file 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    # Filter directly in SQL using LIKE for better performance
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
     for state in rows:
         print(state)
-    cursor.close()
-    db.close()
