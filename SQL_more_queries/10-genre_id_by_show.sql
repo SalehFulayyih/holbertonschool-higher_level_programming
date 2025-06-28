e51fe43730eeb8c35b75ab5aac639fc74c882d11
SalehFulayyih <saleh.5050@hotmail.com>
2025-06-28 09:56:27 +0000

    Add file 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- List all shows that have at least one linked genre
+-- Display: tv_shows.title, tv_show_genres.genre_id
+-- Sort ascending by title and genre_id
+-- Only one SELECT statement allowed
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
