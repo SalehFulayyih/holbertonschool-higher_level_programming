e16f273e459b2fef199d621154339705c0395104
SalehFulayyih <Saleh.5050@hotmail.com>
2025-07-26 06:52:17 +0000

    Add file 100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-magic_string = __import__('100-magic_string').magic_string
-
-for i in range(10):
-    print(magic_string())
+def magic_string(_list=[]):
+    _list.append("BestSchool")
+    return ", ".join(_list)
